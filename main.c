f2334ba09c74b2cd7a43f90d6e38035a870ac3cf
Raymond Burkholder <raymond@burkholder.net>
2021-11-13 19:15:04 +0000

    main.c - add register and value defines

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-#
-# Read Bosch BME680 registers and compute values
-# author:  raymond@burkholder.net
-# started: 2021/11/10
-#
+//
+// Read Bosch BME680 registers and compute values
+// author:  raymond@burkholder.net
+// started: 2021/11/10
+//
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -23,42 +23,156 @@
   https://elinux.org/Interfacing_with_I2C_Devices
   https://www.kernel.org/doc/html/v5.4/i2c/dev-interface.html
   https://gist.github.com/JamesDunne/9b7fbedb74c22ccc833059623f47beb7
+
+  datasheet:
+  https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme680-ds001.pdf
 */
 
 typedef unsigned char   u8;
 
-int fd_i2c;
-int id_adapter = 2;
-u8 addr = 0x76; /* The I2C address */
+// bme680 defines, from rev 1.6 of data sheet
+
+// page 14 - control register in section 5.3.1.3
+// sleep mode
+#define op_mode_sleep  0x00
+// triggers single measurement
+#define op_mode_forced 0x01
+
+// page 29
+#define ctrl_op_mode           0x74
+
+// page 30
+#define ctrl_chip_id           0xd0
+
+// page 15 - suggested oversampling - use one write command - 0x00 means do not perform
+// first osrs_h
+#define oversample_humidity    0x01
+// second osrs_t - section 5.3.2.2
+#define oversample_temperature 0x02
+// third osrs_p
+#define oversample_presssure   0x10
+
+// ignore gas for now - but outline is on page 15
+
+// page 16 has flow chart for all readings
+
+// page 17 - section 3.3.1 - 
+// temperature calculation formula
+
+// page 18 - registers for value temperature retrieval:
+#define calibration_parm_t1_msb 0xea
+#define calibration_parm_t1_lsb 0xe9
+#define calibration_parm_t2_msb 0x8b
+#define calibration_parm_t2_lsb 0x8a
+#define calibration_parm_t3     0x8c
+#define temp_adc_hi             0x22
+#define temp_adc_mid            0x23
+#define temp_adc_lo             0x24
+// page 30 section 5.3.2.2
+#define temperature_control     0x74
+
+// page 19 - registers for value pressure retrieval:
+#define calibration_parm_p1_msb 0x8f
+#define calibration_parm_p1_lsb 0x8e
+#define calibration_parm_p2_msb 0x91
+#define calibration_parm_p2_lsb 0x90
+#define calibration_parm_p3     0x92
+#define calibration_parm_p4_msb 0x95
+#define calibration_parm_p4_lsb 0x94
+#define calibration_parm_p5_msb 0x97
+#define calibration_parm_p5_lsb 0x96
+#define calibration_parm_p6     0x99
+#define calibration_parm_p7     0x98
+#define calibration_parm_p8_msb 0x9d
+#define calibration_parm_p8_lsb 0x9c
+#define calibration_parm_p9_msb 0x9f
+#define calibration_parm_p9_lsb 0x9e
+#define calibration_parm_pa     0xa0
+#define pressure_adc_msb        0x1f
+#define pressure_adc_mid        0x20
+#define pressure_adc_lsb        0x21
+// page 31 section 5.3.2.3
+#define pressure_control        0x74
+
+// page 20 - registers for value humidity retrieval:
+#define calibration_parm_h1_msb 0xe3
+#define calibration_parm_h1_lsb 0xe2
+#define calibration_parm_h2_msb 0xe1
+#define calibration_parm_h2_lsb 0xe2
+#define calibration_parm_h3     0xe4
+#define calibration_parm_h4     0xe5
+#define calibration_parm_h5     0xe6
+#define calibration_parm_h6     0xe7
+#define calibration_parm_h7     0xe8
+#define humidity_adc_msb        0x25
+#define humidity_adc_lsb        0x26
+// page 30 section 5.3.2.1
+#define humidity_control        0x72
+
+// page 31 section 5.3.2.4 - IIR filter control
+//    temperature & pressure
+#define iir_filter_control      0x75
+
+// page 22 - registers for value heater:
+#define heater_parm_g1          0xed
+#define heater_parm_g2_msb      0xec
+#define heater_parm_g2_lsb      0xeb
+#define heater_parm_g3          0xee
+#define heater_range            0x02
+#define heater_value            0x00
+
+// page 23 - registers for value gas:
+#define gas_adc_msb             0x2a
+#define gas_adc_lsb             0x2b
+#define gas_range               0x2b
+#define gas_range_sw_error      0x04
+
+// page 36 - status registers
+#define measurement_status      0x1d
+
+// end bme680 definitions
+
+int open_i2c_bus( int id_bus ) {
+
+  char filename[20];
+  int fd;
+
+  snprintf( filename, 19, "/dev/i2c-%d", id_bus );
+  fd = open( filename, O_RDWR);
+  return fd;
+}
 
 void main() {
 
-  char filename[20];
+  int i2c_id_bus = 2; /* beagleboard bus number */
+  u8  i2c_addr_bme680 = 0x76; /* seeed bme680 board */
+
+  int fd_bme680; /* file descripter for device when opened */
+
   u8 buf[0x20];
 
-  snprintf( filename, 19, "/dev/i2c-%d", id_adapter );
-  fd_i2c = open( filename, O_RDWR);
-  if ( fd_i2c < 0 ) {
+  fd_bme680 = open_i2c_bus( i2c_id_bus );
+  if ( 0 > fd_bme680 ) {
     /* ERROR HANDLING; you can check errno to see what went wrong */
-    printf( "file error\n" );
-  exit(1);
+    printf( "file error (%s)\n", fd_bme680 );
+    exit(1);
   }
 
-  if ( 0 > ioctl( fd_i2c, I2C_SLAVE, addr) ) {
+  if ( 0 > ioctl( fd_bme680, I2C_SLAVE, i2c_addr_bme680) ) {
     /* ERROR HANDLING; you can check errno to see what went wrong */
     printf( "ioctl error\n" );
    exit(1);
   } 
 
   // chip id of BME680 in register 0xd0
-  u8 reg = 0xd0;
-  if ( 0 > write( fd_i2c, &reg, 1 ) ) {
+  u8 reg = ctrl_chip_id;
+  if ( 0 > write( fd_bme680, &reg, 1 ) ) {
     printf( "write failed\n" );
     exit( 1 );
   }
 
   u8 result;
-  if ( 0 > read( fd_i2c, &result, 1 ) ) {
+  if ( 0 > read( fd_bme680, &result, 1 ) ) {
     printf( "read failed\n" );
     exit( 1 );
   }
@@ -70,12 +184,13 @@ void main() {
     printf( "unexpected chip id = 0x%02x", result );
   }
    
-//  if ( i2c_read( id_adapter, 0xD0, buf ) < 0 ) {
-//  }
-//  else {
-//    printf( "result=0x%02x", buf[0] );
-//  }
-
   printf( "done\n" );
 }
+
+  // this does not appear to work, the device does not seem to understand I2C_RDWR
+  // https://gist.github.com/JamesDunne/9b7fbedb74c22ccc833059623f47beb7
+
+  // resorted to simple read/write commands
+  // write with the register desired, then
+  // read for the desired data
 
